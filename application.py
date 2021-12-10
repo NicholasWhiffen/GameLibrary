fefe720aed37ac43419e0617e524a9b54df920c9
Nicholas <nicholaswhiffen@hotmail.com>
2021-12-10 11:46:03 -0330

    "-finished css -created requirements.txt"

--- application.py
+++ application.py
@@ -175,12 +175,3 @@ def delete(id):
         c.execute('DELETE FROM Collections WHERE game_id = (?)', str(id))
         conn.commit()
     return redirect("/collection")
-
-
-def validate_image(stream):
-    header = stream.read(512)
-    stream.seek(0)
-    format = imghdr.what(None, header)
-    if not format:
-        return None
-    return '.' + (format if format != 'jpeg' else 'jpg')
